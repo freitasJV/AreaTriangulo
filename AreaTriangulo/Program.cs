fc38ca9de358469fa452f2a9e93577223cb8707e
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 14:30:54 -0300

    Add project files.

---
+++ AreaTriangulo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+            
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            x.CalcularArea();
+            y.CalcularArea();
+
+            Console.WriteLine($"Área de x = {x.Area.ToString("F4", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Área de x = {y.Area.ToString("F4", CultureInfo.InvariantCulture)}");
+
+            if (x.Area > y.Area)
+                Console.WriteLine("Maios área: X");
+            else if (x.Area == y.Area)
+                Console.WriteLine("Os dois triângulos possuem a mesma área");
+            else
+                Console.WriteLine("Maior área: Y");
+        }
+    }
+}
---
+++ AreaTriangulo/Triangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AreaTriangulo
+{
+    public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+        public double Area;
+
+        public void CalcularArea()
+        {
+            double p;
+            p = (A + B + C) / 2;
+
+            Area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+
+}
